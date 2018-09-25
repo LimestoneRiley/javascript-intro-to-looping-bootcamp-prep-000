fbffaeca6dc2636fe888c488a3bc200696fa2733
Robert Riley <rob_a_riley@yahoo.com>
2018-09-25 17:07:40 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forloop (array){
   for (let i=0; 1<25; i++)
     if (i = 1){
-      array.push "I am 1 strange loop");
+      array.push ("I am 1 strange loop");
     }else{
-      
+      array.push "I am ${i} strange loops."
     }
     
 }
