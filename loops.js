3bfa1cdcd3ba8c20e1ec81b9bf5101b44a5818fa
Robert Riley <rob_a_riley@yahoo.com>
2018-09-25 17:30:12 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -8,4 +8,10 @@ function forLoop (array){
   return (array);
 }
 
-function whileLoop
+function whileLoop(number){
+  while (number > 0){
+    console.log(number);
+    --number;
+  }
+  return ("done");
+}
