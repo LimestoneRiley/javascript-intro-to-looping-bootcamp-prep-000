8b8ad03cec2118e502673ac526092e420fa257c8
Robert Riley <rob_a_riley@yahoo.com>
2018-09-25 17:23:03 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,10 +2,8 @@ function forLoop (array){
   for (let i=0; i<=25; i++)
     if (i == 1){
       array.push ("I am 1 strange loop.");
-      window.alert (array);
     }else{
       array.push ("I am ${i} strange loops.");
-      window.alert (array);
     }
   return (array);
 }
